2215f2f3646b1f628fec5cbb3ad9e3cc3a6074a3
Patrick Strzelec <patrick.strzelec@gmail.com>
2018-04-25 11:40:20 -0400

    updated schema and resolvers to expose entry mutations

--- src/resolvers/Mutation/prismaForwarded.ts
+++ src/resolvers/Mutation/prismaForwarded.ts
@@ -2,5 +2,15 @@ import { getUserId, Context } from "../../utils"
 const { forwardTo } = require("prisma-binding")
 
 export default {
-  createEntry: forwardTo("db")
+  createEntry: forwardTo("db"),
+  updateUser: forwardTo("db"),
+  updateEntry: forwardTo("db"),
+  deleteUser: forwardTo("db"),
+  deleteEntry: forwardTo("db"),
+  upsertUser: forwardTo("db"),
+  upsertEntry: forwardTo("db"),
+  updateManyUsers: forwardTo("db"),
+  updateManyEntries: forwardTo("db"),
+  deleteManyUsers: forwardTo("db"),
+  deleteManyEntries: forwardTo("db")
 }
